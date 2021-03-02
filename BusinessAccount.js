d4e068ab4fc512aee704af533bc9bc5b6e8177ae
Kpieper552 <k.pieper@novi-education.nl>
2021-03-02 15:27:36 +0100

    karinaOOP

--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,62 @@
-// Export your class here as module.exports = MyClass
-module.exports = null;
+// Export your class here as module.exports = MyClassclass BusinessAccount {
+const Account = require("./Account")
+class BusinessAccount extends Account {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        super(name,balance, creditLimit)
+        this.sepaPermission = sepaPermission;
+    }
+
+    makeWithdrawal(amount) {
+        if (amount < this.balance + this.creditLimit && amount < 20000) {
+            this.balance = this.balance - amount;
+
+        }
+    }
+    sepaInvoice(amount) {
+        if (amount > 0 && this.sepaPermission === true) {
+             this.balance = this.balance + (amount * 99/100);
+        }
+    }
+}
+
+
+module.exports = BusinessAccount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
